f324b81d304b3db0939c871fedd5e5b7f34bae39
Fernando Mora <f.mora.g90@gmail.com>
2022-07-15 12:05:46 -0400

    feat(wasm): follow redirects

--- src/wasm/client.rs
+++ src/wasm/client.rs
@@ -185,7 +185,7 @@ async fn fetch(req: Request) -> crate::Result<Response> {
     let mut init = web_sys::RequestInit::new();
     init.method(req.method().as_str());
 
-    init.redirect(web_sys::RequestRedirect::Manual);
+    init.redirect(web_sys::RequestRedirect::Follow);
 
     // convert HeaderMap to Headers
     let js_headers = web_sys::Headers::new()
